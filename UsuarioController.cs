8eaa2a983ca687a03bc055448b47bd6f68cac4da
MaybeDH <hasselc081@gmail.com>
2023-10-28 21:17:24 -0400

    agrando usuariocontroller

---
+++ UsuarioController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Context;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class EmailController : ControllerBase
+    {
+        private readonly ILogger<EmailController> _logger;
+        private readonly AplicacionContexto _aplicacionContexto;
+        public EmailController(
+            ILogger<EmailController> logger,
+            AplicacionContexto aplicacionContexto)
+        {
+            _logger = logger;
+            _aplicacionContexto = aplicacionContexto;
+        }
+       
+        [Route("")]
+        [HttpPost]
+        public IActionResult PostUsuario(
+            [FromBody] Usuario usuario)
+        {
+            _aplicacionContexto.Usuario.Add(usuario);
+            _aplicacionContexto.SaveChanges();
+            return Ok(usuario);
+        }
+        
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Usuario> GetUsuario()
+        {
+            return _aplicacionContexto.Usuario.ToList();
+        }
+       
+        [Route("id")]
+        [HttpPut]
+        public IActionResult PutUsuario([FromBody] Usuario usuario)
+        {
+            _aplicacionContexto.Usuario.Update(usuario);
+            _aplicacionContexto.SaveChanges();
+            return Ok(usuario);
+        }
+       
+        [Route("id")]
+        [HttpDelete]
+        public IActionResult DeleteUsuario(int usuarioID)
+        {
+            _aplicacionContexto.Usuario.Remove(_aplicacionContexto.Usuario.ToList().Where(x => x.idUsuario == usuarioID).FirstOrDefault());
+            _aplicacionContexto.SaveChanges();
+            return Ok(usuarioID);
+        }
+    }
+}
+
+
